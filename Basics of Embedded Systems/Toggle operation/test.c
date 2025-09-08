1ad6b8e0d46cca147dcbf391003742aea39d567c
ESET0074 <Aniket.roy@esyasoft.com>
2025-09-08 17:34:47 +0530

    Add files via upload

---
+++ Basics of Embedded Systems/Toggle operation/test.c
@@ -0,0 +1,28 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define LED_PIN1 PD2
+#define LED_PIN2 PD5
+#define LED_PIN3 PD7
+
+int main(void)
+{
+    DDRD |= (1 << LED_PIN1);
+    DDRD |= (1 << LED_PIN2);
+    DDRD |= (1 << LED_PIN3);
+    
+    PORTD |= (1 << LED_PIN1);
+    PORTD &= ~(1 << LED_PIN2);
+    
+    
+    _delay_ms(2000);
+    PORTD &= ~(1 << LED_PIN1);
+    PORTD |= (1 << LED_PIN2);
+
+    while (1)
+    {
+        
+        PORTD ^=(1<<LED_PIN3);
+        _delay_ms(200);
+}
+}
