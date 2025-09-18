4505b952613644ea2bcac3a6320821de758b97ee
ESET0074 <Aniket.roy@esyasoft.com>
2025-09-18 13:03:01 +0530

    Add files via upload
    Added second assignment of .NET

---
+++ Basics of .Net/Class Assignment/DataStructureDemo/Program.cs
@@ -0,0 +1,111 @@
+﻿namespace DataStructureDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //List Example
+            List<Student> students = new List<Student>();
+
+            // Add Student objects to the list
+            Student first = new Student(1, "Alice", 10);
+            Student second = new Student(2, "Bob", 90);
+            Student third = new Student(3, "Charlie", 78);
+            students.Add(first);
+            students.Add(second);
+            students.Add(third);
+
+
+            // Access and display each student using foreach
+            Console.WriteLine("Student List:");
+            foreach (Student s in students)
+            {
+                Console.WriteLine($"ID: {s.id}, Name: {s.name}, Marks: {s.marks}");
+                Console.WriteLine();
+            }
+
+            // Access a specific object by index
+            Console.WriteLine($"\nSecond student is: {students[1].name}");
+            Console.WriteLine();
+
+
+            //Dictionay Example
+            Dictionary<string, Student> students_dict = new Dictionary<string, Student>();
+            students_dict.Add("firstStudent", first);
+            students_dict.Add("seondStudent", second);
+            students_dict.Add("thirdStudent", third);
+
+
+            foreach (KeyValuePair<string, Student> student in students_dict)
+            {
+                Console.WriteLine($"ID: {student.Value.id}, Name:{student.Value.name},Marks:{student.Value.marks}");
+                Console.WriteLine();
+            }
+
+
+            //Hashset Example
+            Console.WriteLine("Hashset demo");
+            HashSet<Student> students_hashset = new HashSet<Student>();
+            students_hashset.Add(first);
+            students_hashset.Add(second);
+            students_hashset.Add(first);
+            students_hashset.Add(third);
+
+            foreach (Student student in students_hashset)
+            {
+                Console.WriteLine($"ID: {student.id}, Name:{student.name},Marks:{student.marks}");
+                Console.WriteLine();
+            }
+
+
+            //StackDemo
+
+
+            Stack<Student> students_stack = new Stack<Student>();
+            students_stack.Push(first);
+            students_stack.Push(second);
+            students_stack.Push(third);
+
+            Student pop_stack = students_stack.Pop();
+            Console.WriteLine($"ID: {pop_stack.id}, Name:{pop_stack.name},Marks:{pop_stack.marks}");
+            Console.WriteLine();
+
+
+            //Queue demo
+            Console.WriteLine("Queue demo");
+            Queue<Student> students_queue = new Queue<Student>();
+            students_queue.Enqueue(first);
+            students_queue.Enqueue(second);
+            students_queue.Enqueue(third);
+
+            Console.WriteLine($"ID: {students_queue.Dequeue().id}, Name:{students_queue.Dequeue().name},Marks:{students_queue.Dequeue().marks}");
+
+            Console.WriteLine("Linked List demo");
+            LinkedList<Student> students_linkedlist = new LinkedList<Student>();
+            students_linkedlist.AddLast(first);
+            students_linkedlist.AddLast(second);
+            students_linkedlist.AddLast(third);
+
+            foreach (Student student in students_linkedlist)
+            {
+                Console.WriteLine($"ID: {student.id}, Name:{student.name},Marks:{student.marks}");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Tubple demo");
+            Console.WriteLine();
+            var student_tuple = (first, second, third);
+
+            
+            Console.WriteLine($"ID: {student_tuple.Item1.id}, Name:{student_tuple.Item1.name},Marks:{student_tuple.Item1.marks}");
+            Console.WriteLine();
+            Console.WriteLine($"ID: {student_tuple.Item2.id}, Name:{student_tuple.Item2.name},Marks:{student_tuple.Item2.marks}");
+            Console.WriteLine();
+            Console.WriteLine($"ID: {student_tuple.Item3.id}, Name:{student_tuple.Item3.name},Marks:{student_tuple.Item3.marks}");
+
+            
+
+        }
+    }
+}
